66b10b5046f5c771670e6a7d18554481f0d4186d
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-10-04 17:58:23 +0200

    Added random for random number sort

--- Controller.py
+++ Controller.py
@@ -1,12 +1,18 @@
 from PyQt5 import QtWidgets, uic
 import sys
 from Model import Model
+import random
 
 
 class Controller(QtWidgets.QWidget):
     def __init__(self):
         super(Controller, self).__init__()
         self.form = uic.loadUi('gui.ui', self)
+        self.model = Model(15, 0, 0, 0, 0)
+
+    def start_game(self):
+        game_list = self.model.get_game_numbers()
+        random.shuffle(game_list)
 
 
 if __name__ == "__main__":
