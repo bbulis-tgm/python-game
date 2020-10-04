bb258e7fdcabc771c75597283954005889fcc366
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-10-04 15:19:23 +0200

    Added Game.py File

---
+++ Game.py
@@ -0,0 +1,15 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+
+class Game(QtWidgets.QWidget):
+    def __init__(self):
+        super(Game, self).__init__()
+        uic.loadUi('gui.ui', self)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Game()
+    window.show()
+    app.exec()
